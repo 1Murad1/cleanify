95cc5462746c701c782aa3b5b323f2510f15215a
1Murad <muradabdulaev1998borec@gmail.com>
2020-09-11 07:56:30 +0300

    adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,26 @@
 $('.slider').slick({
-    autoPlay: true,
-    autoplaySpeed: 2000,
-    arrows: true
+    arrows: true,
+    responsive: [
+        {
+            breakpoint: 1023,
+            settings: {
+                dots: true,
+                arrows: false
+           }
+        }
+    ]
+});
+
+
+$(window).on('load resize', function() {
+    if ($(window).width() < 1024) {
+        $('.slider_icon').slick({
+            dots: true,
+            arrows: false,
+            slidesToShow: 2,
+            slidesToScroll: 2
+        });
+    }
 });
 
 $(window).on("load", function() {
@@ -9,4 +28,20 @@ $(window).on("load", function() {
         offset: 60
     })
     wow.init();
-})
+});
+
+const btn = document.getElementById("read_more");
+btn.addEventListener("click", function (e) {
+   const hide_bg = document.getElementById("hide_bg");
+   const more = document.getElementById("more");
+   if(hide_bg.style.display === "none") {
+       hide_bg.style.display = "inline";
+       e.target.innerHTML= "Read More";
+       more.style.display ="none"
+   } else {
+       hide_bg.style.display = "none";
+       e.target.innerHTML= "hide";
+       more.style.display ="inline"
+   }
+
+});
